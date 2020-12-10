ebd7d0c071b2022d50e82552680d2c68bb6c578d
boostup <boostup@gmail.com>
2020-12-10 10:09:11 +0100

    new saved albums section in the user library page

--- backend/helpers/getAllPaginatedData.js
+++ backend/helpers/getAllPaginatedData.js
@@ -3,39 +3,71 @@ const spotifyUtils = require("./spotify.config");
 const spotifyApi = spotifyUtils.spotifyApi;
 
 const defaultOptions = {
-    initialData: [],
-    offset: 0,
-    limit: 50,
-    fields: undefined
-}
+  initialData: [],
+  offset: 0,
+  limit: 50,
+  fields: undefined,
+};
 
-module.exports = async function getAllPaginatedData(apiMethodName, resourceId, options) {
+async function getAllPaginatedDataForId(apiMethodName, resourceId, options) {
+  if (!(spotifyApi[apiMethodName] instanceof Function) || !resourceId)
+    throw "Invalid `apiMethodName` or `resourceId` provided";
 
-    if (!(spotifyApi[apiMethodName] instanceof Function) || !resourceId) throw "Invalid `apiMethodName` or `resourceId` provided"
+  try {
+    const _options = {
+      ...defaultOptions,
+      ...options,
+    };
 
-    try {
+    let nextOffset = _options.offset;
+    let items = _options.initialData;
 
-        const _options = {
-            ...defaultOptions,
-            ...options,
-        }
-
-        let nextOffset = _options.offset;
-        let items = _options.initialData;
-
-        while (nextOffset !== null) {
-            const data = await spotifyApi[apiMethodName](resourceId, {
-                limit: _options.limit,
-                offset: nextOffset,
-                fields: _options.fields,
-            });
-            items = items.concat(data.body.items);
-            const params = getParamsFromURL(data.body.next);
-            nextOffset = params ? parseInt(params.offset) : null;
-        }
-        return items;
-    } catch (error) {
-        throw error;
+    while (nextOffset !== null) {
+      const data = await spotifyApi[apiMethodName](resourceId, {
+        limit: _options.limit,
+        offset: nextOffset,
+        fields: _options.fields,
+      });
+      items = items.concat(data.body.items);
+      const params = getParamsFromURL(data.body.next);
+      nextOffset = params ? parseInt(params.offset) : null;
     }
-
+    return items;
+  } catch (error) {
+    throw error;
+  }
 }
+
+async function getAllPaginatedData(apiMethodName, options) {
+  if (!(spotifyApi[apiMethodName] instanceof Function))
+    throw "Invalid `apiMethodName`";
+
+  try {
+    const _options = {
+      ...defaultOptions,
+      ...options,
+    };
+
+    let nextOffset = _options.offset;
+    let items = _options.initialData;
+
+    while (nextOffset !== null) {
+      const data = await spotifyApi[apiMethodName]({
+        limit: _options.limit,
+        offset: nextOffset,
+        fields: _options.fields,
+      });
+      items = items.concat(data.body.items);
+      const params = getParamsFromURL(data.body.next);
+      nextOffset = params ? parseInt(params.offset) : null;
+    }
+    return items;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getAllPaginatedData,
+  getAllPaginatedDataForId,
+};
--- src/_redux/user-library-page/async-actions.js
+++ src/_redux/user-library-page/async-actions.js
@@ -1,17 +1,49 @@
 import { all, takeLatest, call, put, select } from "redux-saga/effects";
 import { userLibraryPageActionTypes as actionTypes } from "./types";
-import { setMyPlaylists } from "./actions";
+import { setMyPlaylists, setMySavedAlbums } from "./actions";
 import { selectSidebarPlaylists } from "../sidebar/selectors";
+import { getToken } from "utils/localStorage";
+import { getMyPlaylistsAync } from "_redux/sidebar/async-actions";
 
 /**
  * WORKER SAGAS
  */
 
+function* getUserSavedAlbums() {
+  try {
+    const { REACT_APP_GET_ALL_SAVED_ALBUMS } = process.env;
+    const url = `${REACT_APP_GET_ALL_SAVED_ALBUMS}?access_token=${getToken()}`;
+    const data = yield call(fetch, url);
+    const albums = yield data.json();
+    yield put(setMySavedAlbums(albums));
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function* getUserPlaylists() {
+  try {
+    const accessToken = getToken();
+    const { REACT_APP_GET_ALL_PLAYLISTS } = process.env;
+
+    const url = `${REACT_APP_GET_ALL_PLAYLISTS}?access_token=${accessToken}`;
+
+    const data = yield call(fetch, url);
+    const playlists = yield data.json();
+    yield put(setMyPlaylists(playlists));
+    return playlists;
+  } catch (error) {
+    throw error;
+  }
+}
+
 function* fetchUserLibraryPageDataStart() {
   try {
-    const playlists = yield select(selectSidebarPlaylists);
-    yield put(setMyPlaylists(playlists));
-
+    yield all([
+      //
+      call(getUserPlaylists),
+      call(getUserSavedAlbums),
+    ]);
     yield put({
       type: actionTypes.FETCH_USER_LIBRARY_PAGE_DATA_SUCCESS,
     });
