9bc6cff7b49c2a981ab97ff67b7f3aee18b5d4ec
boostup <boostup@gmail.com>
2022-03-17 10:07:28 +0100

    New `WelcomeRecruter` component in `LoginPage`

--- src/pages/LoginPage/WelcomeRecruiter.jsx
+++ src/pages/LoginPage/WelcomeRecruiter.jsx
@@ -1,3 +1,5 @@
+import { CopyField } from "components/CopyField";
+
 import Modal from "layout/Modal";
 import { useState } from "react";
 
@@ -10,33 +12,41 @@ function WelcomeRecruiter() {
       buttonLabel={"Ok, got it"}
       onDismiss={() => toggleShow((show) => !show)}
     >
-      <div style={{ maxWidth: "615px" }}>
-        <p>Dear Recruiter,</p>
+      <div className="welcome-recruiters">
+        <p className="title">Dear Visitor,</p>
         <p>&nbsp;</p>
         <p>This app is hooked to Spotify API's.</p>
         <p>&nbsp;</p>
         <p>
           Because each <u>test user must be registered</u>, please use the
-          following registered user:
+          following user:
         </p>
         <p>&nbsp;</p>
-        <div
-          style={{
-            maxWidth: "75%",
-            padding: "1rem",
-            margin: "0 auto",
-            // textAlign: "left",
-          }}
-        >
-          <p>account: </p>
-          <p>boostupmagenta@gmail.com</p>
-          <p>&nbsp;</p>
-          <p>password: </p>
-          <p>********* </p>
-        </div>
+        <p>account: </p>
+        <p>boostupmagenta@gmail.com</p>
+        <CopyToClipboard />
       </div>
     </Modal>
   );
 }
 
 export default WelcomeRecruiter;
+
+/** Internal Component */
+export const CopyToClipboard = () => {
+  const [out, setOut] = useState("");
+  return (
+    <>
+      <p>&nbsp;</p>
+      <p className="out">&nbsp;{out}</p>
+      <CopyField
+        disabled={true}
+        copyTooltip="Click to copy the password"
+        value={"5NK&Wn_X2@jNJzL"}
+        showTextField={false}
+        buttonColor="secondary"
+        onCopySuccess={() => setOut("Password copied!")}
+      />
+    </>
+  );
+};
